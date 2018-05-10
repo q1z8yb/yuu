17ae75d63328dfbdfd002d5b1c7243785d6adf97
yuu <1146591649@qq.com>
2018-05-10 20:28:17 +0800

    fenzhi

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,31 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+      <input type="text" v-model = "message" @keyup.enter = "handleAdd">
+      <ul>
+        <li v-for="item of list">
+          {{item}}
+        </li>
+      </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      message :'',
+      list :[]
+    }
+  },
+  methods :{
+    handleAdd(e) {
+        this.list.push(e.target.value)
+    }
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
